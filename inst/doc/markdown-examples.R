893bead8651f1764749dfe28f7a9f3c0bddf05ca
Yihui Xie <xie@yihui.name>
2013-06-26 00:00:00 +0000

    version 0.6.1

--- inst/doc/markdown-examples.R
+++ inst/doc/markdown-examples.R
@@ -1,15 +1,25 @@
 
+## ----read-code, include=FALSE--------------------------------------------
+library(knitr)
+opts_chunk$set(tidy = FALSE)  # otherwise \n will cause problems
+read_chunk(system.file('examples', 'markdownExtensions.R', package = 'markdown'),
+           labels = 'md-extensions')
+read_chunk(system.file('examples', 'HTMLOptions.R', package = 'markdown'),
+           labels = 'html-options')
 
 
-## @knitr 
+## ------------------------------------------------------------------------
 library(markdown)
 
 
-## @knitr md-extensions
+## ----md-extensions-------------------------------------------------------
 
 
-## @knitr html-options
+## ----html-options--------------------------------------------------------
 
 
+## ----include=FALSE-------------------------------------------------------
+options(markdown.HTML.header = system.file('misc', 'vignette.css', package='knitr'),
+        markdown.HTML.options=markdownHTMLOptions(defaults=TRUE))
 
 
